b16c2a86de955ddb93ee417c1ca06c41deeba2cd
g173341 <39141689+g173341@users.noreply.github.com>
2018-05-09 22:41:35 -0300

    trab03

---
+++ trab03/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  HashAssignment
+//
+//  Created by Kaulitz Guimarães on 14/03/18.
+//  Copyright © 2018 Kaulitz Guimarães. All rights reserved.
+//
+
+#include <iostream>
+#include "Control.hpp"
+
+using namespace std;
+int main() {
+   Control *c = new Control();
+    return 0;
+}
